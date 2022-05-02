2951f3d7fc629fd8b36565e5f5c426b3c5639cb4
Андрій Шевельов <ashevelevmti@gmail.com>
2022-05-02 09:45:19 +0300

    add hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from './container/container';
 import Form from './form/form';
 import ContactsList from './contactsList/contactsList';
 import Filter from './filterContacts/filter';
 // import fechAPI from "../fechAPI/fech";
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filterEl, setFilterEl] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     const localSt = JSON.parse(window.localStorage.getItem('contactsList'));
     if (localSt) {
-      this.setState({ contacts: localSt });
+      setContacts(localSt);
     }
-  }
+  }, []);
 
-  componentDidUpdate() {
-    window.localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
-  }
+  useEffect(() => {
+    if (contacts.length > 0) {
+      window.localStorage.setItem('contactsList', JSON.stringify(contacts));
+    }
+  }, [contacts]);
 
-  addValidContacts = value => {
-    const contacts = this.state.contacts;
+  const addValidContacts = value => {
     const arrey = [...contacts, value];
     if (
       contacts.every(e => e.name !== value.name) &&
       contacts.every(e => e.number !== value.number)
     ) {
-      console.log(value);
-      this.setState({ contacts: arrey });
+      setContacts(arrey);
     } else {
       alert(`"${value.name}" is already in contact!`);
     }
   };
 
-  onInputFilter = value => {
-    this.setState({ filter: value.trim() });
+  const onInputFilter = value => {
+    setFilterEl(value.trim());
   };
 
-  filterVisibleEl = () => {
-    const { contacts, filter } = this.state;
-    const filterLowCace = filter.toLowerCase();
-
-    return contacts.filter(el => {
-      return el.name.toLowerCase().includes(filterLowCace);
-    });
+  const filterVisibleEl = () => {
+    const filterLowCace = filterEl.toLowerCase();
+    return contacts.filter(el => el.name.toLowerCase().includes(filterLowCace));
   };
 
-  removeContacts = id => {
-    this.setState(preState => {
-      return { contacts: preState.contacts.filter(el => el.id !== id) };
-    });
+  const removeContacts = id => {
+    setContacts(prevContats => prevContats.filter(el => el.id !== id));
   };
 
-  render() {
-    const contactsList = this.filterVisibleEl();
-    console.log(contactsList);
-    return (
-      <Container>
-        <div>
-          <Form onSubmit={this.addValidContacts} />
-          <Filter onInputFilter={this.onInputFilter} />
-        </div>
-        <ContactsList contacts={contactsList} removeContacs={this.removeContacts} />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <div>
+        <Form onSubmit={addValidContacts} />
+        <Filter onInputFilter={onInputFilter} />
+      </div>
+      <ContactsList contacts={filterVisibleEl()} removeContacs={removeContacts} />
+    </Container>
+  );
 }
-
-export default App;
